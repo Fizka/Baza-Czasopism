fc0461c544cc1ce78e5853b9bb42176255f6a22a
@Fizka <olaluzaczka@wp.pl>
2020-12-10 18:10:22 +0100

    routing, page model

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { CzasopismaDashboardComponent } from './Komponenty/czasopisma-dashboard/czasopisma-dashboard.component';
+import { MenuComponent } from './Page/menu/menu.component';
+import { FooterComponent } from './Page/footer/footer.component';
+import {AppRoutingModule} from './app-routing.module';
+import {ReactiveFormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CzasopismaDashboardComponent
+    CzasopismaDashboardComponent,
+    MenuComponent,
+    FooterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    ReactiveFormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
